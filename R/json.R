0d198a8c5cd197e6f81f0bb95640f5b783712019
Anthony North <anthony.jl.north@gmail.com>
2020-11-25 22:06:17 +1000

    bugfix: keep attributes for to_json.list

--- R/json.R
+++ R/json.R
@@ -1,47 +1,63 @@
 
 #' To JSON
 #'
-#' Modify `x` for JSON serialisation.
+#' Modify `obj` for JSON serialisation.
 #'
 #' @keywords internal
 #' @noRd
-to_json <- function(x) {
+to_json <- function(obj) {
   UseMethod("to_json")
 }
 
-to_json.default <- function(x) x
+to_json.default <- function(obj) obj
 
-to_json.rdeck <- function(x) {
-  to_json.list(x)
-}
+to_json.rdeck <- function(obj) {
+  rdeck_data <- utils::modifyList(
+    obj$x,
+    list(
+      props = to_json(props(obj)),
+      layers = to_json(layers(obj))
+    ),
+    keep.null = TRUE
+  )
 
-to_json.list <- function(x) {
-  structure(
-    lapply(x, function(p) to_json(p)),
-    class = class(x)
+  utils::modifyList(
+    obj,
+    list(x = rdeck_data),
+    keep.null = TRUE
   )
 }
 
-to_json.layer <- function(x) {
-  names(x) <- snakecase::to_lower_camel_case(names(x))
-  if (!is.null(x$data)) {
-    x$data <- layer_data(x)
+to_json.list <- function(obj) {
+  obj_json <- structure(
+    lapply(obj, function(p) to_json(p)),
+    class = class(obj)
+  )
+
+  mostattributes(obj_json) <- attributes(obj)
+  obj_json
+}
+
+to_json.layer <- function(obj) {
+  names(obj) <- snakecase::to_lower_camel_case(names(obj))
+  if (!is.null(obj$data)) {
+    obj$data <- layer_data(obj)
   }
 
-  to_json.list(x)
+  to_json.list(obj)
 }
 
-to_json.accessor <- function(x) {
-  names(x) <- snakecase::to_lower_camel_case(names(x))
-  to_json.list(x)
+to_json.accessor <- function(obj) {
+  names(obj) <- snakecase::to_lower_camel_case(names(obj))
+  to_json.list(obj)
 }
 
-to_json.rdeck_props <- function(x) {
-  names(x) <- snakecase::to_lower_camel_case(names(x))
-  to_json.list(x)
+to_json.rdeck_props <- function(obj) {
+  names(obj) <- snakecase::to_lower_camel_case(names(obj))
+  to_json.list(obj)
 }
 
-to_json.view_state <- function(x) {
-  names(x) <- snakecase::to_lower_camel_case(names(x))
-  to_json.list(x)
+to_json.view_state <- function(obj) {
+  names(obj) <- snakecase::to_lower_camel_case(names(obj))
+  to_json.list(obj)
 }
