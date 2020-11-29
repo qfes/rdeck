8778fe82a617ed4a2d391f1c594d20dd9178bfac
Anthony North <anthony.jl.north@gmail.com>
2020-11-29 19:44:38 +1000

    fix package doc name collision

---
+++ R/deckgl-layers-doc.R
@@ -0,0 +1,10 @@
+#' @name layer
+#' @param data data.frame
+NULL
+
+#' MVT Layer
+#'
+#' @name mvt_layer
+#' @inherit layer
+#' @family layers
+NULL
--- R/rdeck-package.R
+++ R/rdeck-package.R
@@ -1,4 +1,5 @@
 #' @keywords internal
+#' @aliases rdeck-package
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
