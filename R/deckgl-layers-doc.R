09c48efa433985f666b1ac769ea5953f3423bb60
Anthony North <anthony.jl.north@gmail.com>
2020-12-18 14:53:10 +1000

    docs: add dots param

--- R/deckgl-layers-doc.R
+++ R/deckgl-layers-doc.R
@@ -321,6 +321,9 @@ NULL
 #' @title layer_props
 #' @name layer_props
 #' @rdname _layer_props
+#' @param ... Additional parameters that will be forwarded to deck.gl javascript without
+#' validation nor processing. All dots must be named and will be `camelCased` when serialised.
+#' Layers will raise a warning when dots are used, warning class `rdeck_dots_nonempty`.
 #' @param id <`string`> The layer's identifier must be unique for among all layers of the same
 #' type for a map. Defaults to [uuid::UUIDgenerate()], but should be explicitly defined for
 #' updatable layers in a shiny application.
