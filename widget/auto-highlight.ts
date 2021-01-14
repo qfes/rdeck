fcaf9771038f95826cc0b7a2eba5467dc5e17216
Anthony North <anthony.jl.north@gmail.com>
2021-01-14 14:16:52 +1000

    bugfix: fix tooltip for layers with flattened multi-geometries

--- widget/auto-highlight.ts
+++ widget/auto-highlight.ts
@@ -16,10 +16,25 @@ type LayerType = typeof Layer;
 function overrideEncodePickingColor(Layer: LayerType) {
   const _encodePickingColor = Layer.prototype.encodePickingColor;
   Layer.prototype.encodePickingColor = function encodePickingColor(i: number, target = []) {
-    const data = this.props.data;
+    let { data } = this.props;
+    // multi-geometry
     return isDataFrame(data) && Array.isArray(data.indices)
       ? _encodePickingColor(data.indices[i], target)
       : _encodePickingColor(i, target);
+  };
+}
+
+function overridegetPickingInfo(Layer: LayerType) {
+  const _getPickingInfo = Layer.prototype.getPickingInfo;
+  Layer.prototype.getPickingInfo = function getPickingInfo(params) {
+    let info = _getPickingInfo.call(this, params);
+    let { data } = this.props;
+    // multi-geometry
+    if (isDataFrame(data) && Array.isArray(data.indices)) {
+      info.index = data.indices.indexOf(info.index);
+    }
+
+    return info;
   };
 }
 
@@ -34,3 +49,4 @@ const Layers = [
 ] as LayerType[];
 
 Layers.forEach((Layer) => overrideEncodePickingColor(Layer));
+Layers.forEach((Layer) => overridegetPickingInfo(Layer));
