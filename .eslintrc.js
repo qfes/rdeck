d98723fa3666ac9c2aea1bde6d89c31aa9590db2
Anthony North <anthony.jl.north@gmail.com>
2021-03-09 20:43:32 +1000

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: [".eslintrc.js"],
+  extends: [
+    "react-app",
+    "plugin:jsx-a11y/recommended",
+    "plugin:compat/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+};
