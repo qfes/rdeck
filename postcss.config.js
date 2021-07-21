2c72f6ce2b561d0abd36465d9d463c14477d2bd2
Anthony North <anthony.jl.north@gmail.com>
2021-07-21 16:04:29 +1000

    use postcss nesting

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,10 @@ module.exports = {
     require("postcss-import"),
     require("postcss-url"),
     require("postcss-mixins"),
-    require("postcss-preset-env"),
+    require("postcss-preset-env")({
+      features: {
+        "nesting-rules": true,
+      },
+    }),
   ],
 };
