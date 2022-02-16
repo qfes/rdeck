0160113c72e8da7c601408d61a0f343d80fd31b6
Anthony North <anthony.jl.north@gmail.com>
2022-02-16 18:23:19 +1000

    log trans fails on unsupported range. add tests

--- R/scale_trans.R
+++ R/scale_trans.R
@@ -33,12 +33,9 @@ power_trans <- function(exponent = 0.5) {
 #' @details
 #' If `x` is negative, the result is multiplied by -1.
 #'
-#' This transform assumes -- but doesn't enforce -- that the input range
-#' doesn't cross zero. Transforming an input that crosses 0 will give predictable
-#' output, but its inverse will not, due to unsigned 0 (i.e. -1 * log(1) == log(1)).
-#'
-#' For inverse, the sign for input zeros will be assumed negative if there any negative
-#' input, else positive.
+#' This transform requires that the input range doesn't cross zero. Transforming an input
+#' that crosses 0 will succeed and give predictable output, but its inverse will not, due
+#' to unsigned 0 (i.e. -1 * log(1) == log(1)).
 #'
 #' @name log_trans
 #' @param base <`number`> The log base
@@ -46,17 +43,25 @@ power_trans <- function(exponent = 0.5) {
 #' @family transform
 #' @export
 log_trans <- function(base = exp(1)) {
-  force(base)
+  tidyassert::assert(
+    rlang::is_scalar_double(base) ||
+    rlang::is_scalar_integer(base)
+  )
 
   scales::trans_new(
     paste0("log-", base),
     function(x) sign0(x) * log(abs(x), base),
     function(x) {
-      # fixup sign for 0, assumes original domain doesn't cross zero
-      sgn <- sign0(x)
-      sgn[x == 0] <- min(sgn)
+      finite_x <- x[is.finite(x)]
+      # range cannot cross zero
+      tidyassert::assert(
+        !all(c(-1, 1) %in% sign(finite_x)),
+        "input range must not cross zero"
+      )
 
-      sgn * base^abs(x)
+      # absolute value for all finite x
+      abs_x <- replace(abs(x), x == -Inf, -Inf)
+      min(sign0(finite_x)) * base^abs_x
     }
   )
 }
--- tests/testthat/test-trans.R
+++ tests/testthat/test-trans.R
@@ -25,3 +25,37 @@ test_that("power_trans is invertible", {
     -1e2:1e2
   )
 })
+
+test_that("log_trans handles negative input", {
+  trans <- log_trans(base = 2)
+  expect_equal(
+    trans$transform(c(-8, -4, -2, 2, 4, 8)),
+    c(-3:-1, 1:3)
+  )
+
+  trans <- log_trans(base = 10)
+  expect_equal(
+    trans$transform(c(-1000, -100, -10, 10, 100, 1000)),
+    c(-3:-1, 1:3)
+  )
+})
+
+test_that("log_trans is invertible -- strictly positive or negative", {
+  trans <- log_trans()
+  expect_equal(
+    trans$inverse(trans$transform(-3:-1)),
+    -3:-1
+  )
+
+  expect_equal(
+    trans$inverse(trans$transform(3:1)),
+    3:1
+  )
+})
+
+test_that("log_trans inverse throws if domain crosses 0", {
+  trans <- log_trans()
+  expect_error(trans$inverse(trans$transform(-5:0)), NA)
+  expect_error(trans$inverse(trans$transform(0:5)), NA)
+  expect_error(trans$inverse(trans$transform(-5:5)), class = "assert_error")
+})
