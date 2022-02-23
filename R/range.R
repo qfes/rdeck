640da9788b83bfca5c51cf2241cfd29435908526
Anthony North <anthony.jl.north@gmail.com>
2022-02-24 09:38:30 +1000

    add mutable range helpers

---
+++ R/range.R
@@ -0,0 +1,35 @@
+# initialise a continuous range
+continuous_range <- function(limits = NULL) {
+  rng <- scales::ContinuousRange$new()
+  rng$train(limits)
+  rng
+}
+
+
+# initialise a discrete range
+discrete_range <- function(levels = NULL) {
+  rng <- scales::DiscreteRange$new()
+  rng$train(levels)
+  rng
+}
+
+
+# initialise a continuous identity range
+continuous_identity_range <- function(x = NULL) {
+  rng <- ContinuousIdentityRange$new()
+  rng$train(x)
+  rng
+}
+
+# mutable continuous identity range
+ContinuousIdentityRange <- R6::R6Class(
+  "ContinuousIdentityRange",
+  inherit = scales::Range,
+  list(
+    train = function(x) {
+      tidyassert::assert(is.null(x) || is.numeric(x), "discrete data supplied to continuous scale")
+      self$range <- x
+    },
+    reset = function() self$range <- NULL
+  )
+)
