75fd7b2064f004d0a2b11bf0fbbc712b8a16c1ba
Anthony North <anthony.jl.north@gmail.com>
2022-02-23 15:13:02 +1000

    consolidate ramp functions

--- R/ramp.R
+++ R/ramp.R
@@ -1,3 +1,4 @@
+# a linear colour ramp for continuous input
 color_gradient <- function(palette) {
   UseMethod("color_gradient")
 }
@@ -19,6 +20,8 @@ color_gradient.function <- function(palette) {
   }
 }
 
+
+# a linear colour ramp for discrete input
 color_categories <- function(palette) {
   UseMethod("color_categories")
 }
@@ -48,3 +51,33 @@ color_categories.function <- function(palette) {
     ramp(rescale(x, levels))
   }
 }
+
+
+# a linear numeric ramp for continous input
+number_gradient <- function(seq) {
+  UseMethod("number_gradient")
+}
+
+number_gradient.numeric <- function(seq) {
+  tidyassert::assert(is.finite(seq))
+  stats::approxfun(scales::rescale(seq_along(seq)), seq)
+}
+
+number_gradient.integer <- number_gradient.numeric
+
+
+# a linear numeric ramp for discrete input
+number_categories <- function(seq) UseMethod("number_categories")
+
+number_categories.numeric <- function(seq) {
+  seq_pal <- scales::manual_pal(seq)
+
+  function(x) {
+    levels <- get_levels(x)
+    values <- seq_pal(length(levels))
+    ramp <- number_gradient(values[!is.na(values)])
+    ramp(scales::rescale(match(x, levels)))
+  }
+}
+
+number_categories.integer <- number_categories.numeric
