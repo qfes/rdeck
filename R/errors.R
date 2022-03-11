e07a41965e9aed4977d938540a5477216eb85db9
Anthony North <anthony.jl.north@gmail.com>
2022-03-11 13:46:15 +1000

    error wrappers for clearer errors

---
+++ R/errors.R
@@ -0,0 +1,23 @@
+# base rdeck error
+with_rdeck_errors <- function(expr, error_message, error_class = NULL, call = rlang::caller_env()) {
+  rlang::try_fetch(
+    expr,
+    error = function(err) {
+      rlang::abort(
+        error_message,
+        class = c(error_class, "rdeck_error"),
+        call = call,
+        parent = err
+      )
+    }
+  )
+}
+
+# layer create / validate error
+with_layer_create_errors <- function(expr, call = rlang::caller_env()) {
+  with_rdeck_errors(
+    expr,
+    "Failed to create layer",
+    call = call
+  )
+}
