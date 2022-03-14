e3b092fb5faf79562e0766ce85a06b70b3b3712b
Anthony North <anthony.jl.north@gmail.com>
2022-03-14 19:27:57 +1000

    accessor & scale documentation

--- R/doc-proptypes.R
+++ R/doc-proptypes.R
@@ -4,17 +4,28 @@
 #' Accessors map `data` columns to visual representations, primarily colours: `fill`, `line`,
 #' `highlight` and sizes: `radius`, `elevation`, `width`, `height`.
 #'
-#' Accessors support [tidy-eval](https://dplyr.tidyverse.org/articles/programming.html) expressions.
-#' String literals referencing a column aren't supported (this will be interpreted as a literal
-#' string), however you may pass a bare name (`my_col`) or an unquoted variable (`!!my_var` or
-#' `{{my_var}}`).
-#'
 #' On the client, an accessor is translated to a javascript function that retrieves a value from
 #' `data` for each object rendered. Constant columns will work fine, but aren't recommended as
-#' they are less performant on render and bloat file size; prefer a constant value for these
+#' they are less performant on render and bloat file size; prefer a constant _value_ for these
 #' cases.
 #'
 #' See [accessors](https://deck.gl/docs/developer-guide/using-layers#accessors) for details.
+#'
+#' # Tidy-eval
+#' Accessors support [tidy-eval](https://dplyr.tidyverse.org/articles/programming.html) expressions.
+#' Rules for referencing a column vs. a value from the environment is similar to [dplyr::mutate()],
+#' main difference is that _all_ names are interpreted as columns, instead of falling back to the
+#' environment when a column isn't found.
+#'
+#' Like [dplyr::mutate()], using string literals to reference columns isn't supported -- this will be
+#' interpreted as a literal string. You may reference a column with:
+#' - a bare name (e.g. `my_col`)
+#' - a call evaluating [`name`], e.g. [as.name()], [rlang::sym()], etc
+#' - by injecting a variable from the environment containing a `name` with `!!my_var`, or `{{ myvar }}`
+#'
+#' Literal expressions -- except calls evaluating a [`name`] or a [`scale`] -- will be interpreted as
+#' constants and used for all features in a layer. To use the _value_ of a variable from the environment,
+#' use the [injection operators][rlang::topic-inject], e.g. `!!my_constant`.
 #'
 #' # Accessors & Scales
 #' Accessors may be used everywhere that scales can and can perform _almost_ the same function
@@ -35,30 +46,31 @@ NULL
 #' Scales transform input data into visual representations, predominantly colours:
 #' `fill`, `line`, `highlight` and sizes: `radius`, `elevation`, `width`, `height`.
 #'
-#' Columns referenced by a scale support string literals and
-#' [tidy-eval](https://dplyr.tidyverse.org/articles/programming.html) expressions. Columns
-#' may be bare names (`my_col`), string literals (`"my_col"`), or unquoted expressions
-#' (`!!my_var` or `{{my_var}}`).
-#'
 #' Transformations are performed in client-side javascript, thus scales have very minimal
 #' file-size overhead; what you _pay_ for is the source data being scaled, not the scale
 #' itself. Additional scales referring the same column only _pay_ for that column once.
 #' Adding fields being scaled to the tooltips have minimal overhead also, as this
 #' data is already required for the scale.
 #'
-#' Data retrieved in the browser dynamically can also be scaled. The R interface for scaling
-#' dynamic data is identical to scaling `data.frame` columns, but there is no R-validation for
-#' this (i.e. your scale can refer a non-existent column, which will error in javascript, but
-#' not in R).
+#' Data retrieved in the browser dynamically can also be scaled (including [mvt_layer()]). The R
+#' interface for scaling dynamic data is identical to scaling `data.frame` columns, but there is
+#' no R-validation for this (i.e. your scale can refer a non-existent column, which will error in
+#' javascript, but not in R).
+#'
+#' # Tidy-eval
+#' Columns referenced by a scale support [tidy-eval](https://dplyr.tidyverse.org/articles/programming.html)
+#' expressions. The `col` parameter for a scale must be a valid argument to [rlang::ensym()] and follows
+#' the same rules. Use the [injection operators][rlang::topic-inject] to pass the value of a name
+#' from the environment, e.g. `!!my_column`.
 #'
 #' # Legend
 #' Scales can optionally produce a colour or numeric legend in the client. Numeric legends give
 #' context for which visual field is being scaled and by what column; colour legends additionally
 #' provide a colour scale and _untransformed_ ticks.
 #'
-#' Legend ticks can be formatted with a format function. This function can completely replace the
-#' input ticks if you wish; the only constraint is the return value is a character vector of the
-#' same length as the number of _unformatted ticks_.
+#' Legend ticks can be formatted with a format function (e.g. [scales::label_number()]). This function can
+#' completely replace the input ticks if you wish; the only constraint is the return value is a character
+#' vector of the same length as the number of _unformatted ticks_.
 #'
 #' Legends are [kepler.gl](https://kepler.gl) inspired.
 #' \if{html}{\out{
