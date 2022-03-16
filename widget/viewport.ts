23954f87ca08e76d44c7f2779d35bf3d3f29eb5a
Anthony North <anthony.jl.north@gmail.com>
2022-03-16 12:09:31 +1000

    bugfix: compute bounds with webmercator

--- widget/viewport.ts
+++ widget/viewport.ts
@@ -1,7 +1,7 @@
-import { InitialViewStateProps, Viewport } from "@deck.gl/core";
+import { InitialViewStateProps, Viewport, WebMercatorViewport } from "@deck.gl/core";
 
-export function getViewState(view: Viewport|InitialViewStateProps) {
-  const viewport = view instanceof Viewport ? view : new Viewport(view);
+export function getViewState(view: Viewport | InitialViewStateProps) {
+  const viewport = view instanceof Viewport ? view : new WebMercatorViewport(view);
 
   return {
     center: viewport.unprojectFlat(viewport.center),
