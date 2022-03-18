9993c3a968e4a4a28a7448060d86a98af69bd129
Anthony North <anthony.jl.north@gmail.com>
2022-03-18 13:01:16 +1000

    blending params constants

--- widget/blending.ts
+++ widget/blending.ts
@@ -1,25 +1,23 @@
 import { default as GL } from "@luma.gl/constants";
 
+const BLENDING_MODES = Object.freeze({
+  additive: {
+    [GL.BLEND]: true,
+    blendFunc: [GL.SRC_ALPHA, GL.DST_ALPHA],
+    blendEquation: GL.FUNC_ADD,
+  },
+  subtractive: {
+    [GL.BLEND]: true,
+    blendFunc: [GL.ONE, GL.ONE_MINUS_DST_COLOR, GL.SRC_ALPHA, GL.DST_ALPHA],
+    blendEquation: [GL.FUNC_SUBTRACT, GL.FUNC_ADD],
+  },
+  normal: {
+    [GL.BLEND]: true,
+    blendFunc: [GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA, GL.ONE, GL.ONE_MINUS_SRC_ALPHA],
+    blendEquation: [GL.FUNC_ADD, GL.FUNC_ADD],
+  },
+});
+
 export function blendingParameters(mode: BlendingMode = "normal") {
-  switch (mode) {
-    case "additive":
-      return {
-        [GL.BLEND]: true,
-        blendFunc: [GL.SRC_ALPHA, GL.DST_ALPHA],
-        blendEquation: GL.FUNC_ADD,
-      };
-    case "subtractive":
-      return {
-        [GL.BLEND]: true,
-        blendFunc: [GL.ONE, GL.ONE_MINUS_DST_COLOR, GL.SRC_ALPHA, GL.DST_ALPHA],
-        blendEquation: [GL.FUNC_SUBTRACT, GL.FUNC_ADD],
-      };
-    case "normal":
-    default:
-      return {
-        [GL.BLEND]: true,
-        blendFunc: [GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA, GL.ONE, GL.ONE_MINUS_SRC_ALPHA],
-        blendEquation: [GL.FUNC_ADD, GL.FUNC_ADD],
-      };
-  }
+  return BLENDING_MODES[mode] ?? BLENDING_MODES.normal;
 }
