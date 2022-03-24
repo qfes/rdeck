afc39c20637b672f8f179bb6ec2ed02fddb7e30c
Anthony North <anthony.jl.north@gmail.com>
2022-03-24 20:51:14 +1000

    update stylebox, fix reexports

--- R/reexport-stylebox.R
+++ R/reexport-stylebox.R
@@ -22,69 +22,85 @@ stylebox::mapbox_satellite
 #' @export
 stylebox::mapbox_satellite_streets
 
-#' @importFrom stylebox mapbox_gallery_decimal
+#' @importFrom stylebox mapbox_navigation_day
 #' @export
-stylebox::mapbox_gallery_decimal
+stylebox::mapbox_navigation_day
 
-#' @importFrom stylebox mapbox_gallery_standard
+#' @importFrom stylebox mapbox_navigation_night
 #' @export
-stylebox::mapbox_gallery_standard
+stylebox::mapbox_navigation_night
 
-#' @importFrom stylebox mapbox_gallery_blueprint
+#' @importFrom stylebox mapbox_navigation_guidance_night
 #' @export
-stylebox::mapbox_gallery_blueprint
+stylebox::mapbox_navigation_guidance_night
 
-#' @importFrom stylebox mapbox_gallery_frank
+#' @importFrom stylebox mapbox_navigation_guidance_day
 #' @export
-stylebox::mapbox_gallery_frank
+stylebox::mapbox_navigation_guidance_day
+
+#' @importFrom stylebox mapbox_gallery_bubble
+#' @export
+stylebox::mapbox_gallery_bubble
+
+#' @importFrom stylebox mapbox_gallery_pencil
+#' @export
+stylebox::mapbox_gallery_pencil
 
 #' @importFrom stylebox mapbox_gallery_streets_japan
 #' @export
 stylebox::mapbox_gallery_streets_japan
 
-#' @importFrom stylebox mapbox_galley_pencil
+#' @importFrom stylebox mapbox_gallery_frank
 #' @export
-stylebox::mapbox_galley_pencil
+stylebox::mapbox_gallery_frank
 
-#' @importFrom stylebox mapbox_galley_bubble
+#' @importFrom stylebox mapbox_gallery_blueprint
 #' @export
-stylebox::mapbox_galley_bubble
+stylebox::mapbox_gallery_blueprint
 
-#' @importFrom stylebox mapbox_galley_minimo
+#' @importFrom stylebox mapbox_gallery_standard
 #' @export
-stylebox::mapbox_galley_minimo
-
-#' @importFrom stylebox mapbox_galley_le_shine
-#' @export
-stylebox::mapbox_galley_le_shine
-
-#' @importFrom stylebox mapbox_gallery_cali_terrain
-#' @export
-stylebox::mapbox_gallery_cali_terrain
-
-#' @importFrom stylebox mapbox_gallery_ice_cream
-#' @export
-stylebox::mapbox_gallery_ice_cream
-
-#' @importFrom stylebox mapbox_gallery_north_star
-#' @export
-stylebox::mapbox_gallery_north_star
-
-#' @importFrom stylebox mapbox_gallery_moonlight
-#' @export
-stylebox::mapbox_gallery_moonlight
+stylebox::mapbox_gallery_standard
 
 #' @importFrom stylebox mapbox_gallery_mineral
 #' @export
 stylebox::mapbox_gallery_mineral
 
-#' @importFrom stylebox mapbox_gallery_moves_map
+#' @importFrom stylebox mapbox_gallery_moonlight
 #' @export
-stylebox::mapbox_gallery_moves_map
+stylebox::mapbox_gallery_moonlight
 
-#' @importFrom stylebox mapbox_gallery_swiss_ski
+#' @importFrom stylebox mapbox_gallery_north_star
 #' @export
-stylebox::mapbox_gallery_swiss_ski
+stylebox::mapbox_gallery_north_star
+
+#' @importFrom stylebox mapbox_gallery_decimal
+#' @export
+stylebox::mapbox_gallery_decimal
+
+#' @importFrom stylebox mapbox_gallery_ice_cream
+#' @export
+stylebox::mapbox_gallery_ice_cream
+
+#' @importFrom stylebox mapbox_gallery_cali_terrain
+#' @export
+stylebox::mapbox_gallery_cali_terrain
+
+#' @importFrom stylebox mapbox_gallery_le_shine
+#' @export
+stylebox::mapbox_gallery_le_shine
+
+#' @importFrom stylebox mapbox_gallery_minimo
+#' @export
+stylebox::mapbox_gallery_minimo
+
+#' @importFrom stylebox mapbox_gallery_basic
+#' @export
+stylebox::mapbox_gallery_basic
+
+#' @importFrom stylebox mapbox_studio_galaxy
+#' @export
+stylebox::mapbox_studio_galaxy
 
 #' @importFrom stylebox uber_dark
 #' @export
