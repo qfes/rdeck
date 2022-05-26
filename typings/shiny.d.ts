1d95c5b6af514d40398c559b6ae098760b10d9d4
Anthony North <anthony.jl.north@gmail.com>
2022-05-27 09:39:56 +1000

    fix shiny typings

--- typings/shiny.d.ts
+++ typings/shiny.d.ts
@@ -1,5 +1,8 @@
-/* exposed global */
-export as namespace Shiny;
+declare namespace Shiny {
+  type Handler<T> = (message: T) => void;
+  type EventPriority = "deferred" | "immediate" | "event";
+  type InputValueOptions = { priority?: EventPriority };
 
-export function addCustomMessageHandler(type: string, handler: (data: any) => void): void;
-export function setInputValue(name: string, value: any, opts?: { priority: "event" }): void;
+  export function addCustomMessageHandler<T>(type: string, handler: Handler<T>): void;
+  export function setInputValue<T>(name: string, value: T, opts?: InputValueOptions): void;
+}
