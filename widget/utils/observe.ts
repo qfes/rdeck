48e584e9a8ceb8c76abb7e9c0a136f868f104825
Anthony North <anthony.jl.north@gmail.com>
2022-06-20 16:45:02 +1000

    add observe chaining, preserve property enumerability

--- widget/utils/observe.ts
+++ widget/utils/observe.ts
@@ -1,50 +1,24 @@
-export function observable<T extends object>(object: T, observer?: (info: Partial<T>) => void): T {
-  const onSet = (property: keyof T, value: T[typeof property]) =>
-    // @ts-ignore
-    observer?.({ [property]: value });
-
-  const defineProperty = (target: T, property: keyof T, { value }: PropertyDescriptor) =>
-    Reflect.defineProperty(target, property, {
-      enumerable: true,
-      configurable: false,
-      get() {
-        return value;
-      },
-      set(newValue) {
-        if (Object.is(newValue, value)) return;
-        value = newValue;
-        onSet(property, newValue);
-      },
-    });
-
-  const defineSet = (target: T, property: keyof T, descriptor: PropertyDescriptor) =>
-    Reflect.defineProperty(target, property, {
-      ...descriptor,
-      enumerable: true,
-      configurable: false,
-      set(newValue) {
-        const value = descriptor.get?.call(target);
-        if (Object.is(newValue, value)) return;
-        descriptor.set?.call(target, newValue);
-        onSet(property, newValue);
-      },
-    });
-
-  const getDescriptors = <T>(ob: T) =>
-    Object.entries(Object.getOwnPropertyDescriptors(ob)) as [keyof T, PropertyDescriptor][];
-
+export function observable<T extends object>(object: T, observer?: SetHandler<Partial<T>>): T {
   // replace all writable properties with get() & set()
-  for (const [property, descriptor] of getDescriptors(object)) {
-    if (descriptor.configurable && descriptor.writable) {
-      defineProperty(object, property, descriptor);
+  const ownDescriptors = getDescriptors(object);
+  for (const [property, descriptor] of ownDescriptors) {
+    if (!descriptor.configurable) continue;
+
+    if (descriptor.writable) {
+      defineAccessor(object, property, descriptor, observer);
+    } else if (typeof descriptor.set === "function") {
+      wrapSetter(object, property, descriptor, observer);
     }
   }
 
+  // is this a custom object?
+  if (Object.getPrototypeOf({}) === Object.getPrototypeOf(object)) return object;
+
+  const prototypeDescriptors = getDescriptors(Object.getPrototypeOf(object));
   // wrap all setters on prototype
-  for (const [property, descriptor] of getDescriptors(Object.getPrototypeOf(object))) {
-    if (typeof descriptor.set === "function") {
-      // @ts-ignore
-      defineSet(object, property, descriptor);
+  for (const [property, descriptor] of prototypeDescriptors) {
+    if (descriptor.configurable && typeof descriptor.set === "function") {
+      wrapSetter(object, property as keyof T, descriptor, observer);
     }
   }
 
@@ -55,4 +29,47 @@ export type ChangeHandler = () => void;
 
 export interface Observable {
   onChange?: ChangeHandler;
+}
+
+type DescriptorEntry<T> = [keyof T, PropertyDescriptor];
+function getDescriptors<T extends object>(object: T): DescriptorEntry<T>[] {
+  return Object.entries(Object.getOwnPropertyDescriptors(object)) as DescriptorEntry<T>[];
+}
+
+type SetHandler<T> = (info: T) => void;
+
+function defineAccessor<T extends object, K extends keyof T>(
+  target: T,
+  property: K,
+  { value }: TypedPropertyDescriptor<T[K]>,
+  onSet?: SetHandler<Pick<T, K>>
+) {
+  return Reflect.defineProperty(target, property, {
+    get(): T[K] {
+      // @ts-ignore
+      return value;
+    },
+    set(newValue: T[K]) {
+      if (Object.is(newValue, value)) return;
+      value = newValue;
+      onSet?.({ [property]: newValue } as Pick<T, K>);
+    },
+  });
+}
+
+function wrapSetter<T extends object, K extends keyof T>(
+  target: T,
+  property: K,
+  descriptor: TypedPropertyDescriptor<T[K]>,
+  onSet?: SetHandler<Pick<T, K>>
+) {
+  return Reflect.defineProperty(target, property, {
+    ...descriptor,
+    set(newValue: T[K]) {
+      const value = descriptor.get?.call(target);
+      if (Object.is(newValue, value)) return;
+      descriptor.set?.call(target, newValue);
+      onSet?.({ [property]: newValue } as Pick<T, K>);
+    },
+  });
 }
